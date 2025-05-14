6c1d068eb2db31f5fd9f991e0481b4c21cddc4cf
zesopoulosnikos <zesopoulosnikos@gmail.com>
2025-05-14 22:33:03 +0300

    feat: add gitignore, readme and hello world class

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# my-project8
+# **Requirements**
+
+---
+
+-JDK17
+
+# **Usage**
+
+---
+
+1. Just run the HelloWorld class of the app.
---
+++ src/gr/aueb/cf/helloapp/HelloWorld.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.helloapp;
+
+/**
+ *This project created to show how
+ * to make a git project. And then how
+ * to stage and commit changes in local repo.
+ * And then how to push to GitHub.
+ */
+
+public class HelloWorld {
+}
